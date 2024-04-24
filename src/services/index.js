b38798dcf46adccdc8ee28151fd57f54136defcd
Daniel Trstansky <daniel.trstansky@nov.sk>
2024-04-24 07:35:37 +0200

    delete projects and class

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,7 @@
 import { render } from './render/render.js'
 
-import { projects } from './projects/projects.js'
-
-import { template } from './template/template.js'
-
 export const services = (app) => {
   app.configure(render)
-
-  app.configure(projects)
-
-  app.configure(template)
 
   // All services will be registered here
 }
